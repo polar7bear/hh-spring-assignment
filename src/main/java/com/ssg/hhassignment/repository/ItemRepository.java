1b804e227946cc0b3f00a8145f79b1dac920a7b7
polar7bear <sons1998@naver.com>
2024-09-09 11:15:49 +0900

    add annotation

--- src/main/java/com/ssg/hhassignment/repository/ItemRepository.java
+++ src/main/java/com/ssg/hhassignment/repository/ItemRepository.java
@@ -2,6 +2,8 @@ package com.ssg.hhassignment.repository;
 
 import com.ssg.hhassignment.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
 }
