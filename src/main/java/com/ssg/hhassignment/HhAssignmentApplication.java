cc341cf169b84ef36be3b1fb1bfd4c02b63cbb8e
polar7bear <sons1998@naver.com>
2024-09-09 10:07:28 +0900

    project init

---
+++ src/main/java/com/ssg/hhassignment/HhAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.ssg.hhassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HhAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ssg/hhassignment/HhAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssg.hhassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
